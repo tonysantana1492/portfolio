3fe1eb7471df643355a296e198848b50ee336770
Tony Santana <tonysantana1492@gmail.com>
2025-10-13 10:59:12 -0400

    refactor(): remove loading component and associated metadata

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Loading } from "@/components/shared/loading";
-
-export const metadata = {
-  title: "Loading...",
-};
-
-export default function LoadingPage() {
-  return <Loading className="h-screen" />;
-}
